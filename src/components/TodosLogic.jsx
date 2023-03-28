335f553a14b8793f75e2d63e261e05f44fa576dc
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-03-29 02:07:40 +0300

    Add text input

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,21 +1,22 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import InputTodo from './InputTodo';
 import TodosList from './TodoList';
 
 const TodosLogic = () => {
   const [todos, setTodos] = useState([
     {
-      id: 1,
+      id: uuidv4(),
       title: 'Setup development environment',
       completed: true,
     },
     {
-      id: 2,
+      id: uuidv4(),
       title: 'Develop website and add content',
       completed: false,
     },
     {
-      id: 3,
+      id: uuidv4(),
       title: 'Deploy to live server',
       completed: false,
     },
@@ -36,9 +37,18 @@ const TodosLogic = () => {
       ...todos.filter((todo) => todo.id !== id),
     ]);
   };
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
   return (
     <div>
-      <InputTodo />
+
+      <InputTodo addTodoItem={addTodoItem} />
       <TodosList
         todosProps={todos}
         handleChange={handleChange}
