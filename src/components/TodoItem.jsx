5d2c0238a126e341455e03c67d7c0d0d9a0e0c32
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-03-28 10:33:09 +0300

    Update files

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,11 @@
+const TodoItem = ({ itemProp, handleChange }) => (
+  <li>
+    <input
+      type="checkbox"
+      checked={itemProp.completed}
+      onChange={() => handleChange(itemProp.id)}
+    />
+    {itemProp.title}
+  </li>
+);
+export default TodoItem;
