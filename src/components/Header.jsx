11b1e3b378caaeaa131f8fb64ea130d1df9a659d
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-03-29 02:31:20 +0300

    Add styling modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
+import styles from '../styles/Header.module.css';
+
 const Header = () => (
-  <header>
+  <header className={styles.header}>
     <h1>todos</h1>
     <p>Items will persist in the browser local storage</p>
   </header>
